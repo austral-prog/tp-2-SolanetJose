2c10cd01ed3517f9d0edecc59263cd007cfc9bef
SolanetJose <jopytr123@duck.com>
2025-03-28 10:14:58 -0300

    Update earth.py
    Jose Solanet

--- earth.py
+++ earth.py
@@ -1,3 +1,11 @@
 def earth():
     x = "Bangladesh"
     y = "Barbados"
+    a=("x">"y")
+    b=("x"<"y")
+    sentence_1=(f"The result of {x} comes first in the dictionary than {y} is")
+    sentence_2=(f"The result of {y} comes first in the dictionary than {x} is")
+    first=f"{sentence_1} {b}"
+    second=f"{sentence_2} {a}"
+    print(first+ ".")
+    print(second+ ".")
