2c73cd90e3a1e89635e525c4a2e8a91447c8aa51
SolanetJose <jopytr123@duck.com>
2025-03-28 10:16:19 -0300

    Update change.py
    Jose Solanet Prog 1 TP2

--- change.py
+++ change.py
@@ -1,3 +1,15 @@
 def change():
     expense = 23.75
     money = 100
+    print("Ingresar gasto:")
+    print(expense)
+    print("Dinero recibido")
+    print(money)
+    print("\n" +"Vuelto")
+    print("\n" +"Pesos:")
+    exact=(money-expense)
+    pesos=int(exact)
+    print(pesos)
+    print("Centavos:")
+    centavos=(exact-pesos)
+    print(int(centavos*100))
