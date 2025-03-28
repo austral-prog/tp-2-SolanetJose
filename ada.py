66b87ba0ecea440ea83f7590b1bd5828cf3b77ae
SolanetJose <jopytr123@duck.com>
2025-03-28 10:17:35 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,3 +1,9 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
+    full_name=f"{first_name} {last_name}"
+    print(full_name.lower())
+    print(full_name.title())
+    print(full_name.upper())
+    new_name="\t"+f"{full_name.lower()}"
+    print(new_name)
